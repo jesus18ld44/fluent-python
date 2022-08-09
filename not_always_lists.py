6449eb4367f0b88d12844bb49626d39f52a37124
Jesus <jarevalo80@alumno.uned.es>
2022-08-09 21:21:11 +0200

    measured time of fromfile

--- not_always_lists.py
+++ not_always_lists.py
@@ -12,6 +12,7 @@
 ## as well additional methods for fast loading and saving
 from array import array
 from random import random
+import time
 
 floats = array('d', (random() for i in range(10**7)))
 
@@ -22,8 +23,13 @@ print(floats[-1])
 
 fp = open('floats.bin', 'rb')
 floats2 = array('d')
+
+start = time.time()
 floats2.fromfile(fp, 10**7)
 
 fp.close()
 
+time_fromfile = time.time() - start
+
 print(floats2[-1])
+print(f'time of fromfile function --> {time_fromfile}')
