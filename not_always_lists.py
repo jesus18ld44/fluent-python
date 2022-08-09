292606e2006acbe6ba6070ddb08837c580dff1dd
Jesus <jarevalo80@alumno.uned.es>
2022-08-09 20:58:01 +0200

    updated

--- not_always_lists.py
+++ not_always_lists.py
@@ -18,4 +18,12 @@ floats = array('d', (random() for i in range(10**7)))
 fp = open('floats.bin', 'wb')
 floats.tofile(fp)
 fp.close()
+print(floats[-1])
 
+fp = open('floats.bin', 'rb')
+floats2 = array('d')
+floats2.fromfile(fp, 10**7)
+
+fp.close()
+
+print(floats2[-1])
